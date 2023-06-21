28e449b510eba9d603ca1061b1410091e7ae603d
Alberto Alfaro <aalfaro88@gpofinalpa.com>
2023-06-21 14:46:49 -0600

    Lab almost done

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,33 +1,56 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0
+    this.intervalId = null
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      printTimeCallback(this.currentTime);
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 60)
+    return minutes
   }
 
   getSeconds() {
-    // ... your code goes here
+    if (this.currentTime === 0){
+      return 0
+    }
+    else if (this.currentTime > 59){
+      return this.currentTime % 60
+    }
+    else {
+      return this.currentTime
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    if (value < 10){
+      return "0"+value}
+    else {
+      return (""+value)
+    }
   }
 
+
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
+    this.intervalId = null;
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    let minutes= this.computeTwoDigitNumber(this.getMinutes())
+    let seconds=this.computeTwoDigitNumber(this.getSeconds())
+    return `${minutes}:${seconds}`;
   }
+  
 }
+
